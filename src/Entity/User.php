4fea30d20d3f6bb5bfed1bb5074bcde42834947f
seck <papepolgamb@gmail.com>
2020-02-05 21:46:13 +0100

    quatrimee depot

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,14 +2,42 @@
 
 namespace App\Entity;
 
+use App\Entity\Images;
 use Doctrine\ORM\Mapping as ORM;
+use App\Controller\UserController;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
- * @ApiResource()
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ApiResource(
+ * collectionOperations={
+ *         "get"={
+ *          "normalization_context"={"groups"={"get"}},},
+ *         "post"={"security"="is_granted(['ROLE_ADMIN_SYST','ROLE_ADMIN'])", "security_message"="Seul ADMIN_SYST peut creer un user",
+ * "controller"=UserController::class}
+ *     },
+ * itemOperations={
+ *     "get"={
+ *          "normalization_context"={"groups"={"get"}},
+ * "security"="is_granted('ROLE_ADMIN_SYST')"},
+ *      "put"={"security"="is_granted(['ROLE_ADMIN_SYST','ROLE_ADMIN'])", "security_message"="Seul ADMIN_SYST peut bloquer un user"}
+ * }
+ *    
+ *     )
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ * @UniqueEntity("username",message="L'identifiant doit etre Unique")
+ * @UniqueEntity("email",message="L'email doit etre Unique")
  */
+
+
+ //AdvancedUserInterface etend UserInterface c'est pk on a pas besoin d'implentation UserInterface
 class User implements UserInterface
 {
     /**
@@ -21,6 +49,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"get", "post"})
      */
     private $username;
 
@@ -31,12 +60,14 @@ class User implements UserInterface
 
     /**
      * @var string The hashed password
+     * @Assert\Length(min=8,minMessage="Le mot de passe doit contenir au minimum 8 caracteres")
      * @ORM\Column(type="string")
      */
     private $password;
 
     /**
      * @ORM\Column(type="boolean")
+     * @Groups({"get", "post"})
      */
     private $isActif;
 
@@ -44,6 +75,44 @@ class User implements UserInterface
      * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="users")
      */
     private $role;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"get", "post"})
+     */
+    private $nomComplete;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $telephone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Partenaire", mappedBy="usercreateur")
+     */
+    private $partenaires;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Compte", mappedBy="usercreateur")
+     */
+    private $comptes;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Depot", mappedBy="user")
+     */
+    private $depots;
+
+    public function __construct()
+    {
+        $this->partenaires = new ArrayCollection();
+        $this->comptes = new ArrayCollection();
+        $this->depots = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -138,6 +207,135 @@ class User implements UserInterface
     public function setRole(?Role $role): self
     {
         $this->role = $role;
+
+        return $this;
+    }
+
+    public function getNomComplete(): ?string
+    {
+        return $this->nomComplete;
+    }
+
+    public function setNomComplete(string $nomComplete): self
+    {
+        $this->nomComplete = $nomComplete;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Partenaire[]
+     */
+    public function getPartenaires(): Collection
+    {
+        return $this->partenaires;
+    }
+
+    public function addPartenaire(Partenaire $partenaire): self
+    {
+        if (!$this->partenaires->contains($partenaire)) {
+            $this->partenaires[] = $partenaire;
+            $partenaire->setUsercreateur($this);
+        }
+
+        return $this;
+    }
+
+    public function removePartenaire(Partenaire $partenaire): self
+    {
+        if ($this->partenaires->contains($partenaire)) {
+            $this->partenaires->removeElement($partenaire);
+            // set the owning side to null (unless already changed)
+            if ($partenaire->getUsercreateur() === $this) {
+                $partenaire->setUsercreateur(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Compte[]
+     */
+    public function getComptes(): Collection
+    {
+        return $this->comptes;
+    }
+
+    public function addCompte(Compte $compte): self
+    {
+        if (!$this->comptes->contains($compte)) {
+            $this->comptes[] = $compte;
+            $compte->setUsercreateur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCompte(Compte $compte): self
+    {
+        if ($this->comptes->contains($compte)) {
+            $this->comptes->removeElement($compte);
+            // set the owning side to null (unless already changed)
+            if ($compte->getUsercreateur() === $this) {
+                $compte->setUsercreateur(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Depot[]
+     */
+    public function getDepots(): Collection
+    {
+        return $this->depots;
+    }
+
+    public function addDepot(Depot $depot): self
+    {
+        if (!$this->depots->contains($depot)) {
+            $this->depots[] = $depot;
+            $depot->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeDepot(Depot $depot): self
+    {
+        if ($this->depots->contains($depot)) {
+            $this->depots->removeElement($depot);
+            // set the owning side to null (unless already changed)
+            if ($depot->getUser() === $this) {
+                $depot->setUser(null);
+            }
+        }
 
         return $this;
     }
